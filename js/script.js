d8d6fdf1677784fb4a1db2eb8b3ba1c9c999d073
Johsam <johsamfcress@gmail.com>
2025-04-13 11:36:36 +0200

    added game functionality only remaining with reseting gameboard

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function game_board(){
     let board = Array(9).fill(''); // making an array with 9 elements and assigning the value of null to each element
     const get_board = () => [...board];
     const make_move = (index, symbol) =>{
-        if(board[index === '']){
+        if(board[index] === ''){
             board[index] = symbol;
             return true;
         }
@@ -32,26 +32,80 @@ function game_board(){
     return {get_board, make_move}
 }
 
-// function start_game(){
-//   const board = document.getElementById("board");
-  
-//   addEventListener('mouseover', ()=>{
-//     const current_player = document.getElementById("player").innerText;
-//     current_player.toLowerCase() === 'x'? 'o' : 'x';
+const board = document.getElementById("board");
+const player_display = document.getElementById("player");
+const {get_board, make_move} = game_board();
+const board_boxes_array = Array.from(document.querySelectorAll('.board-box'));
+const check_win = create_game();
+let player_x = document.getElementById('player-x-score');
+let player_o = document.getElementById('player-o-score');
+let draw = document.getElementById('ties');
+player_x.innerText = 0;
+player_o.innerText = 0;
+draw.innerText = 0;
+
+  //updating the UI
+// function updateUI(){
+//   const board = get_board();
+//   board_boxes_array.forEach((board_box, index)=>{
+//     board_box.textContent = board[index];
 //   });
 // }
+  
 
-function start_game() {
-  const board = document.getElementById("board");
-  const playerDisplay = document.getElementById("player");
 
-  board.addEventListener('click', (e) => {
-     if (e.target.classList.contains('board-box')) {
-      const currentPlayer = playerDisplay.innerText;
-      const nextPlayer = currentPlayer.toLowerCase() === 'x' ? 'O' : 'X';
-      playerDisplay.innerText = nextPlayer;
+  //START FROM HERE
+
+
+  // const gird_boxes_array = Array.from(document.querySelectorAll('.board-box'));
+
+board.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('board-box') || e.target.textContent)return;
+   // const index = gridCells.indexOf(e.target);
+  const current_player = player_display.innerText;
+  const index = board_boxes_array.indexOf(e.target);
+  if(make_move(index, current_player)){
+    const current_board = get_board();
+    board_boxes_array.forEach((box, index) => {
+      box.textContent = current_board[index]; // Write array value to HTML
+    });
+
+    //updating results
+    const winner = check_win(get_board());
+    if(winner){
+      if(winner.toLowerCase() === 'x' ){
+        display_winner(winner);
+        player_x.innerText = Number(player_x.innerText) + 1;
+      }else{
+        display_winner(winner);
+        player_o.innerText = Number(player_o.innerText) + 1;
+      }
+
+      //code to clear the board and restart
+      reset_game();
+    }else{
+
+      //code to clear the board and restart
     }
-  });
+
+    const next_player = current_player.toLowerCase() === 'x' ? 'O' : 'X';
+    player_display.innerText = next_player;
+  }
 }
 
-addEventListener('DOMContentLoaded', start_game);
+);
+
+function display_winner(winner){
+
+}
+
+function reset_game(){
+
+}
+
+document.getElementById("restart-btn").addEventListener('click', ()=>{
+  reset_game();
+  player_x.innerText = 0;
+  player_o.innerText = 0;
+  draw.innerText = 0;
+});
